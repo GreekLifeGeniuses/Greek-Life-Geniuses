d431d708f345d67e2e0b445aaba5dc3080c15fe7
brielleuher <72405974+brielleuher@users.noreply.github.com>
2021-11-30 14:45:40 -0500

    Add files via upload

---
+++ edgedetection.m
@@ -0,0 +1,15 @@
+function Ifinal = edgedetection(img)
+[R,C] = size(img);
+I = img(:,:,2);
+mask = logical(size(I));
+mask(I<115) = 1;
+se = strel('diamond',9);
+mask = imclose(mask,se);
+mask = imopen(mask,se);
+margin = 50; 
+croppedImage = imcrop(mask, [margin, margin, C - 2 * margin, R - 2 * margin]);
+mask = imclearborder(croppedImage);
+Ifinal = logical(padarray(mask, margin));
+Ifinal = imclose(Ifinal,se);
+Ifinal = imfill(Ifinal,'holes');
+end
